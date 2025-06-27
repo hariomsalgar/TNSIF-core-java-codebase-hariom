af9c586813a19b7db9ed77a8c7ff5205976f6096
len <len@lenovo>
2025-06-27 16:02:42 +0530

    hi

---
+++ demo/src/com/tnsif/demo/demoproject.java
@@ -0,0 +1,11 @@
+package com.tnsif.demo;
+
+public class demoproject {
+
+	public static void main(String[] args) {
+		System.out.println("user....name");
+		// TODO Auto-generated method stub
+
+	}
+
+}
