08c5087181348110f9aa191730d7f1d027cf96ad
len <len@lenovo>
2025-07-04 01:06:43 +0530

    day5

---
+++ day5/src/com/tnsif/multiinheritance/state.java
@@ -0,0 +1,28 @@
+package com.tnsif.multiinheritance;
+
+public class state extends country{
+	private String statename;
+	private String language;
+	
+	
+	public String getStatename() {
+		return statename;
+	}
+	public void setStatename(String statename) {
+		this.statename = statename;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	
+	@Override
+	public String toString() {
+		return "state [statename=" + statename + ", language=" + language + ", getCountryname()=" + getCountryname()
+				+ ", getCapital()=" + getCapital() + "]";
+	}
+	
+	
+}
